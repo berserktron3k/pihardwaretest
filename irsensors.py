ec8810b02a2a05be61b8fc69f18e3eef5732e6a9
Brady Hummel <brady@fiftyfeetsmall.com>
2022-10-16 18:07:46 -0500

    test

--- irsensors.py
+++ irsensors.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-
-# Set GPIO to BCM
-GPIO.setmode(GPIO.BCM)
+import time
 
 # Define top and bottom sensors and get result
-def get_top():
+def top_sensor():
     global top
+    GPIO.setmode(GPIO.BCM)
     top_pin = 17
     GPIO.setup(top_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     top = GPIO.input(top_pin)
     return top
-    GPIO.cleanup()
 
-def get_bottom():
+def bottom_sensor():
     global bottom
+    GPIO.setmode(GPIO.BCM)
     bot_pin = 27
     GPIO.setup(bot_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     bottom = GPIO.input(bot_pin)
     return bottom
-    GPIO.cleanup()
--- test.py
+++ test.py
@@ -2,11 +2,15 @@
 import irsensors
 
 try:
+    irsensors.top()
+    irsensors.bottom()
     while True:
-        if not irsensors.get_top():
+        if irsensors.top == True:
             print("top sensor tripped")
-        if not irsensors.get_bottom():
+            irsensors.top = None # this resets it back to null preventing infinite loop
+        if irsensors.bottom == True:
             print("bottom sensor tripped")
+            irsensors.bottom = None # this resets it back to null preventing infinite loop
 
 except KeyboardInterrupt:
     exit()
