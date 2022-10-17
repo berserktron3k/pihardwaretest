316e76dada49ab77d0965e555e7509e45d55779a
Brady Hummel <brady@fiftyfeetsmall.com>
2022-10-16 20:36:51 -0500

    test

--- irsensors.py
+++ irsensors.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python
 import RPi.GPIO as GPIO
-import time
 
-# Define top and bottom sensors and get result
-def top_sensor():
-    global top
+def top():
+    def topcb(channel):
+        global top
+        top = True
+        return top
+
     GPIO.setmode(GPIO.BCM)
     top_pin = 17
     GPIO.setup(top_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    top = GPIO.input(top_pin)
-    return top
+    GPIO.add_event_detect(top_pin, GPIO.FALLING, topcb, bouncetime=1000)
 
-def bottom_sensor():
-    global bottom
+def bottom():
+    def botcb(channel):
+        global bottom
+        bottom = True
+        return bottom
+
     GPIO.setmode(GPIO.BCM)
     bot_pin = 27
     GPIO.setup(bot_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    bottom = GPIO.input(bot_pin)
-    return bottom
+    GPIO.add_event_detect(bot_pin, GPIO.FALLING, botcb, bouncetime=1000)
