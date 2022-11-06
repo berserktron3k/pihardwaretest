2661908de59e23056aa75820f214029d76118c9f
Brady Hummel <brady@fiftyfeetsmall.com>
2022-11-06 12:57:29 -0600

    building attracemode

--- attractmode.py
+++ attractmode.py
@@ -9,10 +9,8 @@ import os
 # Variables
 
 attract = True
-world_x = 1280
-world_y = 1024
-fps = 60
-ani = 4
+fps = 60 # Framerate
+ani = 4 # Animaction Cycles
 black = (0, 0, 0)
 
 # Setup
@@ -29,7 +27,7 @@ try:
     inputs.bot_left()
     inputs.bot_right()
     while attract:
-        if (inputs.top == True OR inputs.bottom == True OR inputs.top_left == True OR inputs.bot_left == True OR inputs.bot_right == True)
+        if (inputs.top == True OR inputs.bottom == True OR inputs.top_left == True OR inputs.bot_left == True OR inputs.bot_right == True):
             pygame.quit()
             try:
                 sys.exit()
