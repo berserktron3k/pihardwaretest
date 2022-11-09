1696247bc68221c7693961551bbb74c3ceee2cdc
Brady Hummel <brady@fiftyfeetsmall.com>
2022-11-09 17:04:51 -0600

    testing

--- attractmode.py
+++ attractmode.py
@@ -12,12 +12,12 @@ import time
 # variables
 
 attract = True
-fps = 60 # Framerate
-ani = 4 # Animaction Cycles
+fps = 60 # framerate
+ani = 4 # animaction Cycles
 clock = pygame.time.Clock()
 attract_song = random.choice(glob.glob("./assets/attractmode/audio/*"))
 black = (0, 0, 0)
-loop = 1
+loop = 0
 
 # setup
 
@@ -26,45 +26,47 @@ pygame.mixer.init()
 
 # run attract mode
 
-try:
-    # check status of buttons and ir sensors
-    inputs.top()
-    inputs.bottom()
-    inputs.top_left()
-    inputs.top_right()
-    inputs.bot_left()
-    inputs.bot_right()
-    # begin main attract mode loop
-    while attract:
-        if (inputs.top == True or inputs.bottom == True or inputs.top_left == True or inputs.bot_left == True or inputs.bot_right == True):
-            try:
-                sys.exit()
-            finally:
-                attract = False
-        # create the graphic display
-        world = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        world.fill(black)
-        pygame.mouse.set_visible(False) # hides the mouse cursor
-        pygame.display.flip() # update the full display surface to the screen
-        clock.tick(fps) # advance animation
-        # this bit loads the background track in the pygame mixer
-        # we play the song at full volume once, wait 120 seconds, 
-        # then play it in a loop at half the volume
-        pygame.mixer.music.load(attract_song)
-        if loop < 2:
-            pygame.music.set_volume(0.8)
-            pygame.mixer.music.play()
-            # this bit makes sure the background music plays
-            while pygame.mixer.music.get_busy():
-                pass
-            loop +=1
-        else:
-            time.sleep(120)
-            pygame.music.set_volume(0.4)
-            pygame.mixer.music.play()
-            # this bit makes sure the background music plays
-            while pygame.mixer.music.get_busy():
-                pass
-            loop +=1
-except KeyboardInterrupt:
-    exit()
+def attractmode():
+    try:
+        # check status of buttons and ir sensors
+        inputs.top()
+        inputs.bottom()
+        inputs.top_left()
+        inputs.top_right()
+        inputs.bot_left()
+        inputs.bot_right()
+        # begin main attract mode loop
+        while attract:
+            if (inputs.top == 0 or inputs.bottom == 0 or inputs.top_left == 1 or inputs.bot_left == 1 or inputs.bot_right == 1):
+                try:
+                    sys.exit()
+                finally:
+                    attract = False
+                    return attract
+            # create the graphic display
+            world = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            world.fill(black)
+            pygame.mouse.set_visible(False) # hides the mouse cursor
+            pygame.display.flip() # update the full display surface to the screen
+            clock.tick(fps) # advance animation
+            # this bit loads the background track in the pygame mixer
+            # we play the song at full volume once, wait 120 seconds, 
+            # then play it in a loop at half the volume
+            pygame.mixer.music.load(attract_song)
+            if loop < 1:
+                pygame.music.set_volume(0.8)
+                pygame.mixer.music.play()
+                # this bit makes sure the background music plays
+                while pygame.mixer.music.get_busy():
+                    pass
+                loop +=1
+            else:
+                time.sleep(120)
+                pygame.music.set_volume(0.2)
+                pygame.mixer.music.play()
+                # this bit makes sure the background music plays
+                while pygame.mixer.music.get_busy():
+                    pass
+                loop +=1
+    except KeyboardInterrupt:
+            exit()
--- test.py
+++ test.py
@@ -9,30 +9,29 @@ try:
     inputs.bot_left()
     inputs.bot_right()
     while True:
-        
-        if inputs.top == True:
+        if inputs.top == 0:
             print("top sensor tripped")
-            inputs.top = None # this resets it back to null preventing infinite loop
+            #inputs.top = None # this resets it back to null preventing infinite loop
         
-        if inputs.bottom == True:
+        if inputs.bottom == 0:
             print("bottom sensor tripped")
-            inputs.bottom = None # this resets it back to null preventing infinite loop
+            #inputs.bottom = None # this resets it back to null preventing infinite loop
         
-        if inputs.top_left == True:
+        if inputs.top_left == 1:
             print("top left button pushed")
-            inputs.top_left = None
+            #inputs.top_left = None
         
-        if inputs.top_right == True:
+        if inputs.top_right == 1:
             print("top right button pushed")
-            inputs.top_right = None
+            #inputs.top_right = None
         
-        if inputs.bot_left == True:
+        if inputs.bot_left == 1:
             print("bottom left button pushed")
-            inputs.bot_left = None
+            #inputs.bot_left = None
         
-        if inputs.bot_right == True:
+        if inputs.bot_right == 1:
             print("bottom right button pushed")
-            inputs.bot_right = None
+            #inputs.bot_right = None
 
 except KeyboardInterrupt:
     exit()
