1d082d6ca3decd7e8f9781da9c819a94f7e56573
Brady Hummel <brady@fiftyfeetsmall.com>
2022-11-09 21:58:18 -0600

    test

--- test_new.py
+++ test_new.py
@@ -11,15 +11,15 @@ print(inputs_new.bot_right_button)
 
 print("now we test dynamically")
 while True:
-    if inputs_new.top_sensor():
+    if inputs_new.top_sensor == True:
         print("top sensor status " + inputs_new.top_sensor)
-    if inputs_new.bot_sensor():
+    if inputs_new.bot_sensor == True:
         print("bot sensor status " + inputs_new.bot_sensor)
-    if inputs_new.top_left_button():
+    if inputs_new.top_left_button == True:
         print("top left button status " + inputs_new.top_left_button)
-    if inputs_new.top_right_button():
+    if inputs_new.top_right_button == True:
         print("top right button status " + inputs_new.top_right_button)
-    if inputs_new.bot_left_button():
+    if inputs_new.bot_left_button == True:
         print("bot left button status " + inputs_new.bot_left_button)
-    if inputs_new.bot_right_button():
+    if inputs_new.bot_right_button == True:
         print("bot right button status " + inputs_new.bot_right_button)
