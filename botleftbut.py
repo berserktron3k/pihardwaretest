7d477beedfc13af353313283240f8cbdcc7a2471
Brady Hummel <brady@fiftyfeetsmall.com>
2022-11-13 17:41:21 -0600

    test

--- botleftbut.py
+++ botleftbut.py
@@ -10,6 +10,6 @@ def bot_left():
         return bot_left
     
     GPIO.setmode(GPIO.BCM)
-    bot_left_pin = 23
+    bot_left_pin = 19
     GPIO.setup(bot_left_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.add_event_detect(bot_left_pin, GPIO.RISING, bot_left_cb, bouncetime=1000)
